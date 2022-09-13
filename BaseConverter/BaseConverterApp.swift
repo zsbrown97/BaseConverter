a12d5b681607b00d697347ed95681e18beefac15
zsbrown97 <zack@jeffandbetsy.net>
2022-09-13 11:22:30 -0500

    first commit

---
+++ BaseConverter/BaseConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  BaseConverterApp.swift
+//  BaseConverter
+//
+//  Created by Zack Brown on 9/13/22.
+//
+
+import SwiftUI
+
+@main
+struct BaseConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
